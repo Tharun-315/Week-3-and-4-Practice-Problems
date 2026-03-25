bb84e9d370eb65cf2e0a0fd8e0dc4ec46950f81d
rt4198-droid <rt4198@srmist.edu.in>
2026-03-26 00:35:31 +0530

    added level1 code

---
+++ level1/src/main/java/org/example/CheckEvenorOdd.java
@@ -0,0 +1,15 @@
+// Program to check whether a number is even or odd
+public class EvenOddCheck {
+    public static void main(String[] args) {
+
+        // Declare and initialize variable
+        int number = 10;
+
+        // Check condition using if-else
+        if (number % 2 == 0) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
